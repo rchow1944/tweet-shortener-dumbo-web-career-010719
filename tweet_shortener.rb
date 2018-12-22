fffd4804750c0626d8d435630ab76916649a9496
Raymond Chow <raychow1944@yahoo.com>
2018-12-22 19:28:40 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,50 @@
 # Write your code here.
+def dictionary 
+  {
+    hello: "hi",
+    to: "2",
+    two: "2",
+    too: "2",
+    for: "4",
+    four: "4",
+    be: "b",
+    you: "u",
+    at: "@",
+    and: "&"
+  }
+end
+
+def word_substituter(tweet)
+  dict = dictionary
+  tweet_arr = tweet.split(' ')
+  tweet_arr.each_with_index do |word, i|
+    if dict.keys.to_s.include? word.downcase
+      binding.pry
+      tweet_arr[i] = dict[word.downcase.to_sym]
+    end
+  end
+  tweet_arr.join(' ')
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
